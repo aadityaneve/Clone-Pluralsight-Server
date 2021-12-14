2b89ee86d1a7293b1412c4c0a909bf1568b9078b
Aditya Neve <aditya.neve@gmail.com>
2021-12-14 12:30:22 +0530

    cors added, PORT made dynamic

--- src/server.js
+++ src/server.js
@@ -1,12 +1,29 @@
 const connect = require('./configs/db');
 const express = require('express');
 const app = express();
+const cors = require("cors");
+const PORT = process.env.PORT || 5001;
+
 app.use(express.json());
+
+app.use(cors());
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+    if (res.method === "OPTIONS") {
+        res.header(
+            "Access-Control-Allow-Methods",
+            "GET, POST, DELETE, PUT, PATCH"
+        );
+        return res.status(200).json({});
+    }
+    next();
+});
 
 const start = async () => {
     await connect();
-    app.listen(3001, () => {
-        console.log('LISTENING ON PORT 3001');
+    app.listen(PORT, () => {
+        console.log(`LISTENING ON PORT ${PORT}`);
     });
 };
 
