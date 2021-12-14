c6ca3d7f99feb9d2df1ed3d35a8c48eb8cd76e09
Aditya Neve <aditya.neve@gmail.com>
2021-12-14 11:03:56 +0530

    routes : beginner, intermediate, advanced

--- src/controllers/course.controller.js
+++ src/controllers/course.controller.js
@@ -12,4 +12,38 @@ router.get('/courses', async (req, res) => {
     }
 });
 
+router.get('/courses/beginner', async (req, res) => {
+    try {
+        const course = await Course.find({ level: 'Beginner' }).lean().exec();
+
+        return res.status(200).json({ status: 'Success', course: course });
+    } catch (e) {
+        return res.status(500).json({ status: 'Failed', message: e.message });
+    }
+});
+
+router.get('/courses/intermediate', async (req, res) => {
+    try {
+        const course = await Course.find({ level: 'Intermediate' })
+            .lean()
+            .exec();
+
+        return res.status(200).json({ status: 'Success', course: course });
+    } catch (e) {
+        return res.status(500).json({ status: 'Failed', message: e.message });
+    }
+});
+
+router.get('/courses/advanced', async (req, res) => {
+    try {
+        const course = await Course.find({ level: 'Advanced' })
+            .lean()
+            .exec();
+
+        return res.status(200).json({ status: 'Success', course: course });
+    } catch (e) {
+        return res.status(500).json({ status: 'Failed', message: e.message });
+    }
+});
+
 module.exports = router;
